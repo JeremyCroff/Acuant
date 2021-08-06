d255a207faca9edeb502eceb815f61a4bafc3ca6
JeremyCroff <JeremyCroff@users.noreply.github.com>
2021-08-06 14:41:56 -0500

    Update geturl.js

--- geturl.js
+++ geturl.js
@@ -2,4 +2,4 @@ const txt = document.getElementById( 'AcuantWebWorkerSource' ).textContent;
 const url = URL.createObjectURL( new Blob( [ txt ], {type : 'text/javascript'} ) );
 const acuantConfig = {
  path: url
-}
+};
